7003cdd1a94741ca29ed1689ddaa43f72ecfb6ca
Romain Neutron <imprec@gmail.com>
2012-05-30 12:11:57 +0200

    Add sami configuration

---
+++ sami_configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in($dir = 'src')
+;
+
+return new Sami($iterator, array(
+    'title'                => 'PHP FFMpeg API',
+    'build_dir'            => __DIR__.'/docs/build/html/API',
+    'cache_dir'            => __DIR__.'/docs/build/html/API/cache',
+));
