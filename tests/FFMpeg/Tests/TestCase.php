9c62a128bf8e2c8cc9423923e3f6b8f183c188e6
Romain Neutron <imprec@gmail.com>
2013-05-12 22:36:41 +0200

    Add FFProbe

--- tests/FFMpeg/Tests/TestCase.php
+++ tests/FFMpeg/Tests/TestCase.php
@@ -27,4 +27,51 @@ class TestCase extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
     }
+
+    public function getFFProbeParserMock()
+    {
+        return $this->getMock('FFMpeg\FFProbe\OutputParserInterface');
+    }
+
+    public function getFFProbeOptionsTesterMock()
+    {
+        return $this->getMock('FFMpeg\FFProbe\OptionsTesterInterface');
+    }
+
+    public function getFFProbeMapperMock()
+    {
+        return $this->getMock('FFMpeg\FFProbe\MapperInterface');
+    }
+
+    public function getFFProbeOptionsTesterMockWithOptions(array $options)
+    {
+        $tester = $this->getFFProbeOptionsTesterMock();
+
+        $tester->expects($this->any())
+            ->method('has')
+            ->will($this->returnCallback(function ($option) use ($options) {
+                return in_array($option, $options);
+            }));
+
+        return $tester;
+    }
+
+    public function getConfigurationMock()
+    {
+        return $this->getMock('Alchemy\BinaryDriver\ConfigurationInterface');
+    }
+
+    public function getFormatMock()
+    {
+        return $this->getMockBuilder('FFMpeg\FFProbe\DataMapping\Format')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function getStreamCollectionMock()
+    {
+        return $this->getMockBuilder('FFMpeg\FFProbe\DataMapping\StreamCollection')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
 }
