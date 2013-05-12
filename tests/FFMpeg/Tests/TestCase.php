a6633070d7389296f47f1871abba414a111e0401
Romain Neutron <imprec@gmail.com>
2013-05-12 16:35:24 +0200

    Add FFMpeg and FFProbe drivers

--- tests/FFMpeg/Tests/TestCase.php
+++ tests/FFMpeg/Tests/TestCase.php
@@ -4,4 +4,10 @@ namespace FFMpeg\Tests;
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    public function getLoggerMock()
+    {
+        return $this->getMockBuilder('Monolog\\Logger')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
 }
