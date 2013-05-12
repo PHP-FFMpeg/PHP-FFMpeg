cddd4f50f1a7ab41c060873affa2441ec230c705
Romain Neutron <imprec@gmail.com>
2013-05-12 19:29:36 +0200

    Add FFProbe utilities

---
+++ src/FFMpeg/FFprobe/Mapper.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of PHP-FFmpeg.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FFMpeg\FFProbe;
+
+use FFMpeg\FFProbe;
+use FFMpeg\FFProbe\DataMapping\Format;
+use FFMpeg\FFProbe\DataMapping\StreamCollection;
+use FFMpeg\FFProbe\DataMapping\Stream;
+use FFMpeg\Exception\InvalidArgumentException;
+
+class Mapper implements MapperInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function map($type, $data)
+    {
+        switch ($type) {
+            case FFProbe::TYPE_FORMAT:
+                return $this->mapFormat($data);
+            case FFProbe::TYPE_STREAMS:
+                return $this->mapStreams($data);
+            default:
+                throw new InvalidArgumentException(sprintf(
+                    'Invalid type `%s`.', $type
+                ));
+        }
+    }
+
+    private function mapFormat($data)
+    {
+        return new Format($data['format']);
+    }
+
+    private function mapStreams($data)
+    {
+        $streams = new StreamCollection();
+
+        foreach ($data['streams'] as $properties) {
+            $streams->add(new Stream($properties));
+        }
+
+        return $streams;
+    }
+}
--- tests/FFMpeg/Tests/TestCase.php
+++ tests/FFMpeg/Tests/TestCase.php
@@ -6,7 +6,24 @@ class TestCase extends \PHPUnit_Framework_TestCase
 {
     public function getLoggerMock()
     {
-        return $this->getMockBuilder('Monolog\\Logger')
+        return $this->getMock('Psr\Log\LoggerInterface');
+    }
+
+    public function getCacheMock()
+    {
+        return $this->getMock('Doctrine\Common\Cache\Cache');
+    }
+
+    public function getFFProbeDriverMock()
+    {
+        return $this->getMockBuilder('FFMpeg\Driver\FFProbeDriver')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function getStreamMock()
+    {
+        return $this->getMockBuilder('FFMpeg\FFProbe\DataMapping\Stream')
             ->disableOriginalConstructor()
             ->getMock();
     }
