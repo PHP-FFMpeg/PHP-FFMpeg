ddf7855aca3d85451de6c2b90c5e1142207608a8
Romain Neutron <imprec@gmail.com>
2013-06-20 17:46:36 +0200

    Add missing file headers

--- src/FFMpeg/Coordinate/AspectRatio.php
+++ src/FFMpeg/Coordinate/AspectRatio.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of PHP-FFmpeg.
  *
- * (c) Alchemy <info@alchemy.fr>
+ * (c) Alchemy <dev.team@alchemy.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
