4f98bb0dac23d8e71613981b966c1adecd623c08
Romain Neutron <imprec@gmail.com>
2013-06-24 16:17:21 +0200

    Add FrameFilters

---
+++ src/FFMpeg/Filters/Frame/FrameFilters.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of PHP-FFmpeg.
+ *
+ * (c) Alchemy <dev.team@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FFMpeg\Filters\Frame;
+
+use FFMpeg\Media\Frame;
+
+class FrameFilters
+{
+    private $frame;
+
+    public function __construct(Frame $frame)
+    {
+        $this->frame = $frame;
+    }
+}
