fde58eaafae5780bff36777ebd765760c4eebe8b
Rémi Heens <remi.heens@gmail.com>
2013-09-02 15:09:18 +0200

    adding presets to x264 format

--- src/FFMpeg/Media/Video.php
+++ src/FFMpeg/Media/Video.php
@@ -23,6 +23,7 @@ use FFMpeg\Media\Frame;
 
 class Video extends Audio
 {
+
     /**
      * {@inheritdoc}
      *
@@ -62,44 +63,66 @@ class Video extends Audio
         $filters = clone $this->filters;
         $filters->add(new SimpleFilter($format->getExtraParams(), 10));
 
-        if ($this->driver->getConfiguration()->has('ffmpeg.threads')) {
+        if($this->driver->getConfiguration()->has('ffmpeg.threads'))
+        {
             $filters->add(new SimpleFilter(array('-threads', $this->driver->getConfiguration()->get('ffmpeg.threads'))));
         }
-        if (null !== $format->getVideoCodec()) {
+        if(null !== $format->getVideoCodec())
+        {
             $filters->add(new SimpleFilter(array('-vcodec', $format->getVideoCodec())));
         }
-        if (null !== $format->getAudioCodec()) {
+        if(null !== $format->getAudioCodec())
+        {
             $filters->add(new SimpleFilter(array('-acodec', $format->getAudioCodec())));
         }
 
-        foreach ($filters as $filter) {
+        foreach($filters as $filter)
+        {
             $commands = array_merge($commands, $filter->apply($this, $format));
         }
 
         $commands[] = '-b:v';
         $commands[] = $format->getKiloBitrate() . 'k';
-        $commands[] = '-refs';
-        $commands[] = '6';
-        $commands[] = '-coder';
-        $commands[] = '1';
-        $commands[] = '-sc_threshold';
-        $commands[] = '40';
-        $commands[] = '-flags';
-        $commands[] = '+loop';
-        $commands[] = '-me_range';
-        $commands[] = '16';
-        $commands[] = '-subq';
-        $commands[] = '7';
-        $commands[] = '-i_qfactor';
-        $commands[] = '0.71';
-        $commands[] = '-qcomp';
-        $commands[] = '0.6';
-        $commands[] = '-qdiff';
-        $commands[] = '4';
-        $commands[] = '-trellis';
-        $commands[] = '1';
 
-        if (null !== $format->getAudioKiloBitrate()) {
+        $presets = null;
+        if($format instanceof \FFMpeg\Format\Video\X264)
+        {
+            $presets = $format->getPreset();
+        }
+        
+        if(is_null($presets))
+        {
+            $commands[] = '-refs';
+            $commands[] = '6';
+            $commands[] = '-coder';
+            $commands[] = '1';
+            $commands[] = '-sc_threshold';
+            $commands[] = '40';
+            $commands[] = '-flags';
+            $commands[] = '+loop';
+            $commands[] = '-me_range';
+            $commands[] = '16';
+            $commands[] = '-subq';
+            $commands[] = '7';
+            $commands[] = '-i_qfactor';
+            $commands[] = '0.71';
+            $commands[] = '-qcomp';
+            $commands[] = '0.6';
+            $commands[] = '-qdiff';
+            $commands[] = '4';
+            $commands[] = '-trellis';
+            $commands[] = '1';
+        }
+        else
+        {
+            // presets defined
+            $commands[] = '-preset';
+            $commands[] = $presets;
+        }
+
+
+        if(null !== $format->getAudioKiloBitrate())
+        {
             $commands[] = '-b:a';
             $commands[] = $format->getAudioKiloBitrate() . 'k';
         }
@@ -123,28 +146,35 @@ class Video extends Audio
 
         $failure = null;
 
-        foreach (array($pass1, $pass2) as $pass => $passCommands) {
-            try {
+        foreach(array($pass1, $pass2) as $pass => $passCommands)
+        {
+            try
+            {
                 /** add listeners here */
                 $listeners = null;
 
-                if ($format instanceof ProgressableInterface) {
-                    $listeners = $format->createProgressListener($this, $this->ffprobe, $pass + 1, 2);
+                if($format instanceof ProgressableInterface)
+                {
+                    $listeners = $format->createProgressListener($this,
+                            $this->ffprobe, $pass + 1, 2);
                 }
 
                 $this->driver->command($passCommands, false, $listeners);
-            } catch (ExecutionFailureException $e) {
+            }
+            catch(ExecutionFailureException $e)
+            {
                 $failure = $e;
                 break;
             }
         }
 
         $this
-            ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log')
-            ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log')
-            ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log.mbtree');
+                ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log')
+                ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log')
+                ->cleanupTemporaryFile(getcwd() . '/' . $passPrefix . '-0.log.mbtree');
 
-        if (null !== $failure) {
+        if(null !== $failure)
+        {
             throw new RuntimeException('Encoding failed', $failure->getCode(), $failure);
         }
 
@@ -161,4 +191,5 @@ class Video extends Audio
     {
         return new Frame($this->pathfile, $this->driver, $this->ffprobe, $at);
     }
+
 }
