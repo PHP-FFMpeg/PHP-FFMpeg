a87522759143aba354fe16d9e4dfdcf097b54613
Romain Neutron <imprec@gmail.com>
2013-11-28 18:49:13 +0100

    Fix tests

--- tests/FFMpeg/Tests/Filters/Video/SynchronizeFilterTest.php
+++ tests/FFMpeg/Tests/Filters/Video/SynchronizeFilterTest.php
@@ -13,6 +13,6 @@ class SynchronizeFilterTest extends TestCase
         $format = $this->getMock('FFMpeg\Format\VideoInterface');
 
         $filter = new SynchronizeFilter();
-        $this->assertEquals(array('-async', '1'), $filter->apply($video, $format));
+        $this->assertEquals(array('-async', '1', '-metadata:s:v:0', 'start_time=0'), $filter->apply($video, $format));
     }
 }
