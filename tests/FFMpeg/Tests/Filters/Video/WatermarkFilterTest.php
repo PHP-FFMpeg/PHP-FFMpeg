863fd1b70fc2996c7ac53fbdb145f0b4fc56062b
Romain Neutron <imprec@gmail.com>
2015-01-30 19:52:57 +0100

    Fix file name

--- tests/FFMpeg/Tests/Filters/Video/WatermarkFilterTest.php
+++ tests/FFMpeg/Tests/Filters/Video/WatermarkFilterTest.php
@@ -19,8 +19,8 @@ class WatermarkFilterTest extends TestCase
 
         $format = $this->getMock('FFMpeg\Format\VideoInterface');
 
-        $filter = new WatermarkFilter(__DIR__ . '/../../files/watermark.png');
-        $this->assertEquals(array('-vf', 'overlay 0:0'), $filter->apply($video, $format));
+        $filter = new WatermarkFilter(__DIR__ . '/../../../../files/watermark.png');
+        $this->assertEquals(array('-vf', 'movie=/Users/romain.viovi/Documents/workspace/PHP-FFMpeg/tests/FFMpeg/Tests/Filters/Video/../../../../files/watermark.png [watermark]; [in][watermark] overlay=0:0 [out]'), $filter->apply($video, $format));
 
         // check size of video is unchanged
         $this->assertEquals(320, $stream->get('width'));
@@ -33,31 +33,31 @@ class WatermarkFilterTest extends TestCase
         $format = $this->getMock('FFMpeg\Format\VideoInterface');
 
         // test position absolute
-        $filter = new WatermarkFilter(__DIR__ . '/../../files/watermark.png', array(
+        $filter = new WatermarkFilter(__DIR__ . '/../../../../files/watermark.png', array(
             'position' => 'absolute',
             'x' => 10, 'y' => 5
         ));
-        $this->assertEquals(array('-vf', 'overlay 10:5'), $filter->apply($video, $format));
+        $this->assertEquals(array('-vf', 'movie=/Users/romain.viovi/Documents/workspace/PHP-FFMpeg/tests/FFMpeg/Tests/Filters/Video/../../../../files/watermark.png [watermark]; [in][watermark] overlay=10:5 [out]'), $filter->apply($video, $format));
 
         // test position relative
-        $filter = new WatermarkFilter(__DIR__ . '/../../files/watermark.png', array(
+        $filter = new WatermarkFilter(__DIR__ . '/../../../../files/watermark.png', array(
             'position' => 'relative',
             'bottom' => 10, 'left' => 5
         ));
-        $this->assertEquals(array('-vf', 'overlay 5:main_h - 10 - overlay_h'), $filter->apply($video, $format));
+        $this->assertEquals(array('-vf', 'movie=/Users/romain.viovi/Documents/workspace/PHP-FFMpeg/tests/FFMpeg/Tests/Filters/Video/../../../../files/watermark.png [watermark]; [in][watermark] overlay=5:main_h - 10 - overlay_h [out]'), $filter->apply($video, $format));
 
         // test position relative
-        $filter = new WatermarkFilter(__DIR__ . '/../../files/watermark.png', array(
+        $filter = new WatermarkFilter(__DIR__ . '/../../../../files/watermark.png', array(
             'position' => 'relative',
             'bottom' => 5, 'right' => 4
         ));
-        $this->assertEquals(array('-vf', 'overlay main_w - 4 - overlay_w:main_h - 5 - overlay_h'), $filter->apply($video, $format));
+        $this->assertEquals(array('-vf', 'movie=/Users/romain.viovi/Documents/workspace/PHP-FFMpeg/tests/FFMpeg/Tests/Filters/Video/../../../../files/watermark.png [watermark]; [in][watermark] overlay=main_w - 4 - overlay_w:main_h - 5 - overlay_h [out]'), $filter->apply($video, $format));
 
         // test position relative
-        $filter = new WatermarkFilter(__DIR__ . '/../../files/watermark.png', array(
+        $filter = new WatermarkFilter(__DIR__ . '/../../../../files/watermark.png', array(
             'position' => 'relative',
             'left' => 5, 'top' => 11
         ));
-        $this->assertEquals(array('-vf', 'overlay 5:11'), $filter->apply($video, $format));
+        $this->assertEquals(array('-vf', 'movie=/Users/romain.viovi/Documents/workspace/PHP-FFMpeg/tests/FFMpeg/Tests/Filters/Video/../../../../files/watermark.png [watermark]; [in][watermark] overlay=5:11 [out]'), $filter->apply($video, $format));
     }
 }
