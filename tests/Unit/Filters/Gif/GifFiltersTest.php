183dd53dfca6a2b75539cebb2f77c73008968b33
Romain Biard <romain@digitallift.fr>
2017-01-17 13:37:47 -0300

    Creation of a media to extract video sequences into gif files

---
+++ tests/Unit/Filters/Gif/GifFiltersTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\FFMpeg\Unit\Filters\Gif;
+
+use Tests\FFMpeg\Unit\TestCase;
+use FFMpeg\Filters\Gif\GifFilters;
+
+class GifFiltersTest extends TestCase
+{
+	
+}
---
+++ tests/Unit/Media/GifTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Tests\FFMpeg\Unit\Media;
+
+use FFMpeg\Media\Gif;
+use FFMpeg\Coordinate\Dimension;
+
+class GifTest extends AbstractMediaTestCase
+{
+    public function testGetTimeCode()
+    {
+        $driver = $this->getFFMpegDriverMock();
+        $ffprobe = $this->getFFProbeMock();
+        $timecode = $this->getTimeCodeMock();
+        $dimension = $this->getDimensionMock();
+
+        $gif = new Gif($this->getVideoMock(__FILE__), $driver, $ffprobe, $timecode, $dimension);
+        $this->assertSame($timecode, $gif->getTimeCode());
+    }
+
+    public function testGetDimension()
+    {
+        $driver = $this->getFFMpegDriverMock();
+        $ffprobe = $this->getFFProbeMock();
+        $timecode = $this->getTimeCodeMock();
+        $dimension = $this->getDimensionMock();
+
+        $gif = new Gif($this->getVideoMock(__FILE__), $driver, $ffprobe, $timecode, $dimension);
+        $this->assertSame($dimension, $gif->getDimension());
+    }
+
+    public function testFiltersReturnFilters()
+    {
+        $driver = $this->getFFMpegDriverMock();
+        $ffprobe = $this->getFFProbeMock();
+        $timecode = $this->getTimeCodeMock();
+        $dimension = $this->getDimensionMock();
+
+        $gif = new Gif($this->getVideoMock(__FILE__), $driver, $ffprobe, $timecode, $dimension);
+        $this->assertInstanceOf('FFMpeg\Filters\Gif\GifFilters', $gif->filters());
+    }
+
+    public function testAddFiltersAddsAFilter()
+    {
+        $driver = $this->getFFMpegDriverMock();
+        $ffprobe = $this->getFFProbeMock();
+        $timecode = $this->getTimeCodeMock();
+        $dimension = $this->getDimensionMock();
+
+        $filters = $this->getMockBuilder('FFMpeg\Filters\FiltersCollection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $filter = $this->getMock('FFMpeg\Filters\Gif\GifFilterInterface');
+
+        $filters->expects($this->once())
+            ->method('add')
+            ->with($filter);
+
+        $gif = new Gif($this->getVideoMock(__FILE__), $driver, $ffprobe, $timecode, $dimension);
+        $gif->setFiltersCollection($filters);
+        $gif->addFilter($filter);
+    }
+
+    /**
+     * @dataProvider provideSaveOptions
+     */
+    public function testSave($dimension, $duration, $commands)
+    {
+        $driver = $this->getFFMpegDriverMock();
+        $ffprobe = $this->getFFProbeMock();
+        $timecode = $this->getTimeCodeMock();
+
+        $timecode->expects($this->once())
+            ->method('__toString')
+            ->will($this->returnValue('timecode'));
+
+        $pathfile = '/target/destination';
+
+        array_push($commands, $pathfile);
+
+        $driver->expects($this->once())
+            ->method('command')
+            ->with($commands);
+
+        $gif = new Gif($this->getVideoMock(__FILE__), $driver, $ffprobe, $timecode, $dimension, $duration);
+        $this->assertSame($gif, $gif->save($pathfile));
+    }
+
+    public function provideSaveOptions()
+    {
+        return array(
+            array(
+                new Dimension(320, 240), 3,
+                array(
+                    '-ss', 'timecode',
+                    '-t', '3',
+                    '-i', __FILE__,
+                    '-vf',
+                    'scale=320:-1', '-gifflags',
+                    '+transdiff', '-y'
+                ),
+            ),
+            array(
+                new Dimension(320, 240), null,
+                array(
+                    '-ss', 'timecode',
+                    '-i', __FILE__,
+                    '-vf',
+                    'scale=320:-1', '-gifflags',
+                    '+transdiff', '-y'
+                )
+            ),
+        );
+    }
+}
