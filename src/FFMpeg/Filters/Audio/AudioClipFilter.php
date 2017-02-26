43cd4331db4f1a64120125ff4c00953a8d65e270
jens1o <jenshausdorf@gmail.com>
2017-02-26 21:33:21 +0100

    prevent audio from being reencoded

--- src/FFMpeg/Filters/Audio/AudioClipFilter.php
+++ src/FFMpeg/Filters/Audio/AudioClipFilter.php
@@ -75,6 +75,9 @@ class AudioClipFilter implements AudioFilterInterface {
             $commands[] = (string) $this->duration;
          }
 
+         $commands[] = '-acodec';
+         $commands[] = 'copy';
+
          return $commands;
      }
 
