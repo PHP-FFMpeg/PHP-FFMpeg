88881052a7ab4c7db512fd5446957c7dc068632d
Alex Sikamiotis <asikamiotis@gmail.com>
2021-04-27 18:46:53 +0300

    Respect format KiloBitRate when using openAdvanced method & map

--- src/FFMpeg/Media/AdvancedMedia.php
+++ src/FFMpeg/Media/AdvancedMedia.php
@@ -259,6 +259,10 @@ class AdvancedMedia extends AbstractMediaType
                 $commands[] = '-vcodec';
                 $commands[] = $format->getVideoCodec();
             }
+            if ($format->getKiloBitrate() !== 0) {
+                $commands[] = '-b:v';
+                $commands[] = $format->getKiloBitrate() . 'k';
+            }
             // If the user passed some additional format parameters.
             if ($format->getAdditionalParameters() !== null) {
                 $commands = array_merge($commands, $format->getAdditionalParameters());
