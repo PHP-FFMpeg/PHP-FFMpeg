5e7b15710a8607e8a3a2d9fbe2c150a99b924fa5
Pascal Baljet <pascal@protone.media>
2024-11-12 16:39:52 +0100

    Support for PHP 8.4 (#933)
    * Add PHP 8.4 to test matrix
    * Refactored `AbstractProcessBuilderFactoryTest` to trait

--- tests/Alchemy/BinaryDriver/NONLTSProcessBuilderFactoryTest.php
+++ tests/Alchemy/BinaryDriver/NONLTSProcessBuilderFactoryTest.php
@@ -3,9 +3,13 @@
 namespace Alchemy\Tests\BinaryDriver;
 
 use Alchemy\BinaryDriver\ProcessBuilderFactory;
+use Alchemy\Tests\BinaryDriver\AbstractProcessBuilderFactoryTrait;
+use Tests\FFMpeg\Unit\TestCase;
 
-class NONLTSProcessBuilderFactoryTest extends AbstractProcessBuilderFactoryTest
+class NONLTSProcessBuilderFactoryTest extends TestCase
 {
+    use AbstractProcessBuilderFactoryTrait;
+
     protected function getProcessBuilderFactory($binary)
     {
         ProcessBuilderFactory::$emulateSfLTS = true;
