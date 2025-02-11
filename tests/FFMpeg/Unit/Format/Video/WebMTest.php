fa3e3b63682e2c1de527ac252112ed198e455494
Ahmed T. Eladl <ahmed.eladl@rub.de>
2025-02-11 23:37:31 +0100

    feat: add libopus support to WebM available audio codecs (#937)

--- tests/FFMpeg/Unit/Format/Video/WebMTest.php
+++ tests/FFMpeg/Unit/Format/Video/WebMTest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Tests\FFMpeg\Unit\Format\Video;
 
 use FFMpeg\Format\Video\WebM;
@@ -9,5 +8,16 @@ class WebMTest extends VideoTestCase
     public function getFormat()
     {
         return new WebM();
+    }
+
+    // Returns array containing exactly three audio codecs
+    public function testGetAvailableAudioCodecs(): void
+    {
+        $webm = new WebM();
+
+        $codecs = $webm->getAvailableAudioCodecs();
+
+        $this->assertCount(3, $codecs);
+        $this->assertEquals(['copy', 'libvorbis', 'libopus'], $codecs);
     }
 }
